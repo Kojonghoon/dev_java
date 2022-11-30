1f7c36ee1061ebf86258f442faaeccde34bc730d
dui7272 <dui7272@gamil.com>
2022-11-30 14:20:33 +0900

    Test2

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -3,7 +3,6 @@ package dev_java.ch01;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Test");
-        
         System.out.println("git 설정 추가");
         System.out.println("Hello Java!!!");
     }
