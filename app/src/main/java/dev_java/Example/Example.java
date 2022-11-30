5b40a13ffccc5ab234337ef56d0997f563820a88
dui7272 <dui7272@gamil.com>
2022-11-30 17:34:17 +0900

    22.11.30

---
+++ app/src/main/java/dev_java/Example/Example.java
@@ -0,0 +1,14 @@
+package dev_java.Example;
+
+public class Example {
+  String i = "Example 입니다.";
+  String j = "Example이 아닙니다.";
+
+  void methodC(){
+    i=i;
+    j=j;
+    System.out.println("i는 "+ i);
+    System.out.println("j는 "+j);
+
+  }
+}
---
+++ app/src/main/java/dev_java/Example/Example2.java
@@ -0,0 +1,10 @@
+package dev_java.Example;
+
+public class Example2 {
+  public static void main(String[] args) {
+
+    Example example = new Example();
+    example.methodC();
+
+  }
+}
