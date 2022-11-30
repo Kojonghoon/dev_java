8aa0f54b4bc5f385e88ce0e66a3c9dddeb7f095f
Kojonghoon <dui7272@gamil.com>
2022-11-30 14:09:37 +0900

    test

---
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -0,0 +1,8 @@
+package dev_java.ch01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("git 설정 추가");
+        System.out.println("Hello Java!!!");
+    }
+}
