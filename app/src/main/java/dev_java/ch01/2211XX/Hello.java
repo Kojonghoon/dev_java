286e8fd48c1c29e172d8858aecb709a58e3f860e
dui7272 <dui7272@gamil.com>
2022-12-01 15:40:51 +0900

    22년12월1일

--- app/src/main/java/dev_java/ch01/2211XX/Hello.java
+++ app/src/main/java/dev_java/ch01/2211XX/Hello.java
@@ -2,15 +2,16 @@ package dev_java.ch01;
 
 public class Hello {
 
-    int methodA(int i, int j){
+    int methodA(int i, int j) {
         int hap;
-        hap = i+j;
+        hap = i + j;
         return hap;
     }
+
     public static void main(String[] args) {
-      Hello hello = new Hello();
-      hello.methodA(2, 3);
-      
+        Hello hello = new Hello();
+        hello.methodA(2, 3);
+
         System.out.println("Test");
         System.out.println("git 설정 추가");
         System.out.println("Hello Java!!!");
