92b18c12ca363acdc714ee66e6e9097d8dcee0df
dui7272 <dui7272@gamil.com>
2022-12-01 17:42:41 +0900

    22년12월01 마무리

--- app/src/main/java/dev_java/Y22M12D01/String1.java
+++ app/src/main/java/dev_java/Y22M12D01/String1.java
@@ -9,11 +9,16 @@ public class String1 {
     // insert here
     // s1과 s2의 주소번지는 같니? T
     // 그 이유 System.out.println(s1 == s2);
-    System.out.println(s1 == s2); //T ==주소값을 비교
-    // s3와 s4의 주소번지가 같니? 다르니? 
-    System.out.println(s3 == s4); //F ==주소값을 비교
-    // 만일 s3이 카르키는 문자열이 s4와 같은지를 비교하려면 어떡하지? 구글링 - System.out.println(s3.equals(s4));
+    System.out.println(s1 == s2); // T ==주소값을 비교
+    System.out.println(s1 == s3); // F
+    System.out.println(s1 == s4); // F
+    System.out.println(s2 == s3); // F
+    System.out.println(s2 == s4); // F
+    System.out.println(s3 == s4); // F
+    // s3와 s4의 주소번지가 같니? 다르니?
+    System.out.println(s3 == s4); // F ==주소값을 비교
+    // 만일 s3이 카르키는 문자열이 s4와 같은지를 비교하려면 어떡하지? 구글링 -
+    // System.out.println(s3.equals(s4));
     System.out.println(s3.equals(s4)); // 문자열 비교
   }
-
 }
