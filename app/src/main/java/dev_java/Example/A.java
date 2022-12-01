c7ddb6638b53e5b4a11b355758bd8d0b30ad78db
dui7272 <dui7272@gamil.com>
2022-12-01 13:40:49 +0900

    22.12.01

---
+++ app/src/main/java/dev_java/Example/A.java
@@ -0,0 +1,23 @@
+package dev_java.Example;
+
+class B {
+  int i;
+}
+
+public class A {
+  void C(B b) {
+    b = new B();
+    b.i = 500;
+    System.out.println("Sub i는 " + b.i + " 입니다.");
+  }
+
+  public static void main(String[] args) {
+    A a = new A();
+    B b = new B();
+    a.C(b);
+    b.i = 100;
+    // a.C(b);
+    System.out.println("Main i는 " + b.i + " 입니다");
+  }
+
+}
---
+++ app/src/main/java/dev_java/ch01/DeptVOMain.java
@@ -0,0 +1,21 @@
+package dev_java.ch01;
+
+public class DeptVOMain {
+  public static void main(String[] args) {
+    DeptVO deptVO = new DeptVO();
+    deptVO.deptno = 10;
+    deptVO.dname = "개발1팀";
+    System.out.println(deptVO.deptno);// 10
+    System.out.println(deptVO.dname);// 개발1팀
+    System.out.println(deptVO.deptno);// 10
+    System.out.println(deptVO);//위치
+    System.out.println("==============");
+    deptVO = new DeptVO();// 변수 선언X, 객체 생성
+    System.out.println(deptVO); // 위치
+    deptVO.deptno = 30;
+    deptVO.dname = "개발2팀";
+    System.out.println(deptVO.deptno);// 30
+    System.out.println(deptVO.dname); // 개발2팀
+    System.out.println(deptVO.loc); // null
+  }
+}
