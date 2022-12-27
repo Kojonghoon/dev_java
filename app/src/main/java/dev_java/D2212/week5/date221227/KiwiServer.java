0915c9de64c70ffc88ee1617f62dc18e5c8fcbae
dui7272 <dui7272@gamil.com>
2022-12-27 17:48:52 +0900

    20221227 8교시

--- app/src/main/java/dev_java/D2212/week5/date221227/KiwiServer.java
+++ app/src/main/java/dev_java/D2212/week5/date221227/KiwiServer.java
@@ -2,7 +2,7 @@ package dev_java.D2212.week5.date221227;
 
 import javax.swing.JFrame;
 
-public class KiwiServer extends JFrame {
+public class KiwiServer extends JFrame implements Runnable {
   // 선언부
 
   // 생성자
@@ -20,5 +20,18 @@ public class KiwiServer extends JFrame {
   public static void main(String[] args) {
     KiwiServer ks = new KiwiServer();
     ks.initDisplay();
+    Thread th = new Thread(ks);
+    th.start();
+  }
+
+  @Override
+  public void run() {
+    System.out.println("run 호출 성공");
+    // 경합이 벌어지는 일
+    // 지연(대기)이 필요한 일
+    // 일반메소드로 처리할 때와 다른 점
+    // 보장받는다. 늦게라도 실행됨. - 일반메소드는 어플리케이션이 종료되면 보장못함
+    // 개별적으로 동작을 할 수 있다.
+
   }
 }
